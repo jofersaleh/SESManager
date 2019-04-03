302aeca79ce3277bc7e814065f40e83b89c63a08
Changbeom Choi <cbchoi@handong.edu>
2019-04-03 20:17:59 +0900

    Initial Commit  - Setup Project Structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='SESManager',
+    version='0.0.1',
+    packages=['system_entity', 'system_manager'],
+    url='https://github.com/cbchoi/SESManager',
+    license='',
+    author='cbchoi',
+    author_email='cbchoi@handong.edu',
+    description=''
+)
