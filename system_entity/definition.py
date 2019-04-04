9997a8fd98efd6c2c5b451f5a93818067caf828b
Changbeom Choi <cbchoi@handong.edu>
2019-04-04 21:33:22 +0900

    Implementation  + AlternativeAttribute  + AlternativeType

--- system_entity/definition.py
+++ system_entity/definition.py
@@ -6,6 +6,7 @@ from enum import Enum
 class AttributeType(Enum):
     BEHAVIOR = 0
     STRUCTURAL = 1
+    ALTERNATIVE = 2
     UNKNOWN_TYPE = -1
 
     @staticmethod
@@ -14,6 +15,8 @@ class AttributeType(Enum):
             return AttributeType.BEHAVIOR
         elif "STRUCTURAL" == name.upper():
             return AttributeType.STRUCTURAL
+        elif "ALTERNATIVE" == name.upper():
+            return AttributeType.ALTERNATIVE
         else:
             return AttributeType.UNKNOWN_TYPE
 
@@ -23,5 +26,36 @@ class AttributeType(Enum):
             return "BEHAVIOR"
         elif enum == AttributeType.STRUCTURAL:
             return "STRUCTURAL"
+        elif enum == AttributeType.ALTERNATIVE:
+            return "ALTERNATIVE"
+        else:
+            return "UNKNOWN"
+
+
+class AlternativeType(Enum):
+    AND = 0
+    OR = 1
+
+    UNKNOWN_TYPE = -1
+
+    @staticmethod
+    def resolve_type_from_str(name):
+        if "BEHAVIOR" == name.upper():
+            return AttributeType.BEHAVIOR
+        elif "STRUCTURAL" == name.upper():
+            return AttributeType.STRUCTURAL
+        elif "ALTERNATIVE" == name.upper():
+            return AttributeType.ALTERNATIVE
+        else:
+            return AttributeType.UNKNOWN_TYPE
+
+    @staticmethod
+    def resolve_type_from_enum(enum):
+        if enum == AttributeType.BEHAVIOR:
+            return "BEHAVIOR"
+        elif enum == AttributeType.STRUCTURAL:
+            return "STRUCTURAL"
+        elif enum == AttributeType.ALTERNATIVE:
+            return "ALTERNATIVE"
         else:
             return "UNKNOWN"
