23a46c35c72820e1431cd85437e0623ef4278103
HanwoolHandong <21400229@handong.edu>
2019-08-02 15:38:10 +0900

    Commit agent sim model

--- sample/sim_model_agent_maneuver.py
+++ sample/sim_model_agent_maneuver.py
@@ -3,6 +3,7 @@ from system_executor.behavior_model_executor import *
 from system_executor.system_message import *
 
 import math
+from random import uniform
 
 class Agent5(BehaviorModelExecutor):
     def __init__(self, instance_time, destruct_time, name, engine_name):
@@ -10,8 +11,11 @@ class Agent5(BehaviorModelExecutor):
 
         self.x = 0
         self.y = 0
-        self.waypoints = [(10,10), (20,20), (30,30), (30,0), (0, 0)]
+        self.waypoints = [(uniform(0,100), uniform(0,100)), (uniform(0,100), uniform(0,100)),
+                          (uniform(0,100), uniform(0,100)), (uniform(0,100), uniform(0,100)),
+                          (uniform(0,100), uniform(0,100))]
         print("waypoint: ", self.waypoints)
+        self.result = []
 
 
         self.init_state("IDLE")
@@ -20,8 +24,10 @@ class Agent5(BehaviorModelExecutor):
         self.insert_state("SEND", 1)
         self.insert_state("REACHED", 1)
 
+        self.insert_input_port("exin")
         self.insert_input_port("received")
         self.insert_output_port("waypoint")
+        self.insert_output_port("exout")
 
 
 
@@ -35,26 +41,38 @@ class Agent5(BehaviorModelExecutor):
 
     def ext_trans(self,port, msg):
         print("2")
-        if port == "received":
+        if port == "exin":
+            print("3")
             self._cur_state = "SEND"
+        elif port == "received":
+            if self.check_done():
+                data = msg.retrieve()
+                self.result.append(data[0])
+                self._cur_state = "SEND"
+            else:
+                self._cur_state = "REACHED"
         else:
             data = msg.retrieve()
             print(data[0])
 
     def output(self):
-        print("5")
-        self._cur_state = "WAIT"
-        location = self.waypoints[0]
-        print(location)
-        msg = SysMessage(self.get_name(), location)
-        print("6")
-        return msg
+        if self._cur_state == "SEND":
+            self._cur_state = "WAIT"
+            location = self.waypoints[0]
+            print(location)
+            msg = SysMessage(self.get_name(), location)
+            print("6")
+            return msg
+        elif self._cur_state == "REACHED":
+            msg = SysMessage(self.get_name(), self.result)
+            print("99")
+            return msg
 
     def int_trans(self):
-        print("4")
         if self._cur_state == "IDLE":
             print("7")
             self._cur_state = "SEND"
+            self.output()
         if self.waypoints is None:
             self._cur_state = "REACHED"
 
@@ -63,8 +81,8 @@ class Agent5(BehaviorModelExecutor):
 class Maneuver(BehaviorModelExecutor):
     def __init__(self, instance_time, destruct_time, name, engine_name):
         BehaviorModelExecutor.__init__(self, instance_time, destruct_time, name, engine_name)
-        self.init_state("IDLE")
-        self.insert_state("IDLE", 0)
+        self.init_state("LISTEN")
+        self.insert_state("LISTEN", 0)
         self.insert_state("MOVE", 1)
         self.insert_state("FINSH", 1)
 
@@ -101,6 +119,7 @@ class Maneuver(BehaviorModelExecutor):
         data = msg.retrieve()
         self.waypoint = data[0]
         self._cur_state = "MOVE"
+        self.move()
 
     def output(self):
         if self._cur_state == "FINISH":
@@ -110,16 +129,21 @@ class Maneuver(BehaviorModelExecutor):
             return msg
 
     def int_trans(self):
-        self.move()
+        #print("77")
+        if self._cur_state == "MOVE":
+            print("88")
+            self.move()
 
 
-h = Agent5(0, 100, "Peter", "sname")
-r = Maneuver(0, 100, "Simon", "sname")
+
+
+h = Agent5(0, 1000, "Peter", "sname")
+r = Maneuver(0, 1000, "Simon", "sname")
 
 with open('./sample/model_db/Gen5.pkl', 'wb') as f:
-    dill.dump(Agent5(0, 100, "Peter", "sname"), f)
+    dill.dump(Agent5(0, 1000, "Peter", "sname"), f)
 with open('./sample/model_db/Maneuver.pkl', 'wb') as f:
-    dill.dump(Maneuver(0, 100, "Simon", "sname"), f)
+    dill.dump(Maneuver(0, 1000, "Simon", "sname"), f)
 
 h = None
 r = None
@@ -136,8 +160,13 @@ SystemSimulator().register_engine("sname")
 #print("!")
 #print(SystemSimulator.get_engine("sname"))
 
+SystemSimulator().get_engine("sname").insert_input_port("in")
 SystemSimulator().get_engine("sname").register_entity(h)
 SystemSimulator().get_engine("sname").register_entity(r)
+SystemSimulator().get_engine("sname").coupling_relation(None, "in", h, "exin")
+SystemSimulator().get_engine("sname").coupling_relation(h, "exout", None, "out")
 SystemSimulator().get_engine("sname").coupling_relation(h, "waypoint", r, "recv")
 SystemSimulator().get_engine("sname").coupling_relation(r, "send_result", h, "received")
+SystemSimulator().get_engine("sname").insert_external_event("in", None)
 SystemSimulator().get_engine("sname").simulate()
+print(SystemSimulator().get_engine("sname").handle_external_output_event())
